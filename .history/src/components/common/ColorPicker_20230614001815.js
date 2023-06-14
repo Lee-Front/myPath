b1598657035e2acca1c746d0b04d8509f5d7e932
wkdrmadl3 <wkdrmadl3@naver.com>
2023-06-15 00:55:55 +0900

    colorPicket 기본값 에러 수정,에디터 불필요 소스 삭제 및 통합

---
+++ .history/src/components/common/ColorPicker_20230614001815.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { SketchPicker } from "react-color";
+
+const ColorPicker = ({ color, handleChange, handleChangeComplete }) => {
+  console.log("color: ", color);
+  return (
+    <PickerContainer className="color-sketch">
+      <SketchPicker
+        color={color}
+        onChange={handleChange}
+        onChangeComplete={handleChangeComplete}
+      />
+    </PickerContainer>
+  );
+};
+
+export default ColorPicker;
+
+const PickerContainer = styled.div`
+  position: absolute;
+  margintop: 0.2rem;
+  top: 100%;
+  left: 0;
+`;
---
+++ .history/src/stores/useEditorStore_20230614181709.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { create } from "zustand";
+
+const useEditorStore = create((set, get) => ({
+  blocks: [],
+  selectedBlocks: [],
+  setBlocks: (blocks) => set((state) => ({ ...state, blocks: blocks })),
+  getBlocks: async (pathId) => {
+    const response = await axios.get("/api/editor", {
+      params: { pathId },
+    });
+    const blocks = response.data;
+
+    blocks.sort(function (a, b) {
+      return a.sort - b.sort;
+    });
+
+    set((state) => ({ ...state, blocks }));
+  },
+  changeBlockStyle: (blockId, style) => {
+    const blocks = get().blocks;
+    const block = blocks.find((block) => block.uuid === blockId);
+    console.log("block?.style : ", block?.style);
+    console.log("style : ", style);
+    block.style = { ...block?.style, ...style };
+
+    set((state) => ({ ...state, blocks }));
+  },
+}));
+
+export default useEditorStore;
