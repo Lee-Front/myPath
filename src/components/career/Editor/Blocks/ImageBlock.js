c2f4df22e540523a5008db04a190e65540c35779
wkdrmadl3 <wkdrmadl3@naver.com>
2023-07-21 23:18:14 +0900

    mac지원을 위한 마우스 관련 이벤트 대거 수정 및 에러 수정

--- src/components/career/Editor/Blocks/ImageBlock.js
+++ src/components/career/Editor/Blocks/ImageBlock.js
@@ -1,87 +1,78 @@
-import React from "react";
-import styled from "@emotion/styled";
-import { useState } from "react";
-import { useEffect } from "react";
+import React from 'react';
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-const ImageBlock = ({ style, data, overlayWidth, changeShowFileUploader }) => {
-  const [fileId, setFileId] = useState();
-  const [width, setWidth] = useState();
+const ImageBlock = ({ style, data, overlayWidth }) => {
+    const [fileId, setFileId] = useState();
+    const [width, setWidth] = useState();
 
-  useEffect(() => {
-    if (data?.files) {
-      setFileId(data?.files[0]?.fileId);
-    }
-  }, [data]);
+    useEffect(() => {
+        if (data?.files) {
+            setFileId(data?.files[0]?.fileId);
+        }
+    }, [data]);
 
-  return (
-    <BlockContainer
-      fileId={fileId}
-      onClick={(e) => {
-        changeShowFileUploader(e);
-      }}
-    >
-      {fileId != null ? (
-        <Image
-          src={`${process.env.REACT_APP_API_URL}/api/common/images/${fileId}`}
-          onLoad={() => {
-            setWidth(data?.files[0]?.width);
-          }}
-          alt="이미지"
-        />
-      ) : (
-        <UploadWrapper>
-          <UploadImage viewBox="0 0 30 30">
-            <path d="M1,4v22h28V4H1z M27,24H3V6h24V24z M18,10l-5,6l-2-2l-6,8h20L18,10z M11.216,17.045l1.918,1.918l4.576-5.491L21.518,20H9 L11.216,17.045z M7,12c1.104,0,2-0.896,2-2S8.104,8,7,8s-2,0.896-2,2S5.896,12,7,12z"></path>
-          </UploadImage>
-          <UploadText>이미지를 등록해주세요.</UploadText>
-        </UploadWrapper>
-      )}
-      {style ? <div style={style}></div> : null}
-    </BlockContainer>
-  );
+    return (
+        <BlockContainer fileId={fileId}>
+            {fileId != null ? (
+                <Image
+                    src={`${process.env.REACT_APP_API_URL}/api/common/images/${fileId}`}
+                    onLoad={() => {
+                        setWidth(data?.files[0]?.width);
+                    }}
+                    alt="이미지"
+                />
+            ) : (
+                <UploadWrapper>
+                    <UploadImage viewBox="0 0 30 30">
+                        <path d="M1,4v22h28V4H1z M27,24H3V6h24V24z M18,10l-5,6l-2-2l-6,8h20L18,10z M11.216,17.045l1.918,1.918l4.576-5.491L21.518,20H9 L11.216,17.045z M7,12c1.104,0,2-0.896,2-2S8.104,8,7,8s-2,0.896-2,2S5.896,12,7,12z"></path>
+                    </UploadImage>
+                    <UploadText>이미지를 등록해주세요.</UploadText>
+                </UploadWrapper>
+            )}
+            {style ? <div style={style}></div> : null}
+        </BlockContainer>
+    );
 };
 
 export default ImageBlock;
 
 const BlockContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex: 1;
-  outline: none;
-  line-height: 1.5;
-  padding: 1rem;
+    width: 100%;
+    display: flex;
+    flex: 1;
+    outline: none;
+    line-height: 1.5;
+    padding: 1rem;
 
-  justify-content: center;
-  background: ${(props) =>
-    props.fileId == null
-      ? "rgba(55, 53, 47, 0.05)"
-      : props.hoverYn
-      ? "rgba(55, 53, 47, 0.08)"
-      : ""};
-  cursor: pointer;
+    justify-content: center;
+    background: ${(props) =>
+        props.fileId == null ? 'rgba(55, 53, 47, 0.05)' : props.hoverYn ? 'rgba(55, 53, 47, 0.08)' : ''};
+    cursor: pointer;
 `;
 const Image = styled.img`
-  width: ${(props) => props.width + "px"};
-  max-width: 100%;
-  transition: 0.5s;
+    width: ${(props) => props.width + 'px'};
+    max-width: 100%;
+    transition: 0.5s;
 `;
 
 const UploadWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
+    width: 100%;
+    display: flex;
+    justify-content: center;
 `;
 
 const UploadImage = styled.svg`
-  flex-shrink: 0;
-  width: 2.5rem;
-  height: 2.5rem;
-  fill: rgba(55, 53, 47, 0.45);
-  backface-visibility: hidden;
-  margin-right: 1.2rem;
+    flex-shrink: 0;
+    width: 2.5rem;
+    height: 2.5rem;
+    fill: rgba(55, 53, 47, 0.45);
+    backface-visibility: hidden;
+    margin-right: 1.2rem;
 `;
 
 const UploadText = styled.span`
-  overflow: hidden;
-  text-overflow: ellipsis;
+    overflow: hidden;
+    text-overflow: ellipsis;
 `;
--- src/components/career/Editor/EditBranchComponent.js
+++ src/components/career/Editor/EditBranchComponent.js
@@ -1,158 +1,117 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React from 'react';
+import styled from '@emotion/styled';
 
-import ImageBlock from "./Blocks/ImageBlock";
-import MultipleBlock from "./Blocks/MultipleBlock";
-import CheckBoxBlock from "./Blocks/CheckBoxBlock";
-import BulletPointBlock from "./Blocks/BulletPointBlock";
-import EditableBlock from "./Blocks/EditableBlock";
-import useEditorStore from "../../../stores/useEditorStore";
+import ImageBlock from './Blocks/ImageBlock';
+import MultipleBlock from './Blocks/MultipleBlock';
+import CheckBoxBlock from './Blocks/CheckBoxBlock';
+import BulletPointBlock from './Blocks/BulletPointBlock';
+import EditableBlock from './Blocks/EditableBlock';
+import useEditorStore from '../../../stores/useEditorStore';
 
-const EditBranchComponent = ({
-  data,
-  movementSide,
-  changeShowFileUploader,
-  overlayWidth,
-  isOverlay,
-}) => {
-  const getMovementStyle = (side) => {
-    const styleObject = {
-      position: "absolute",
-      background: "rgba(35, 131, 226, 0.28)",
-      zIndex: "999",
+const EditBranchComponent = ({ data, movementSide, overlayWidth, isOverlay }) => {
+    const getMovementStyle = (side) => {
+        const styleObject = {
+            position: 'absolute',
+            background: 'rgba(35, 131, 226, 0.28)',
+            zIndex: '999',
+        };
+
+        if (side === 'top') {
+            styleObject.top = 0;
+            styleObject.left = 0;
+            styleObject.width = '100%';
+            styleObject.height = '4px';
+        } else if (side === 'bottom') {
+            styleObject.bottom = 0;
+            styleObject.left = 0;
+            styleObject.width = '100%';
+            styleObject.height = '4px';
+        } else if (side === 'left') {
+            styleObject.top = 0;
+            styleObject.left = 0;
+            styleObject.width = '4px';
+            styleObject.height = '100%';
+        } else if (side === 'right') {
+            styleObject.top = 0;
+            styleObject.right = 0;
+            styleObject.width = '4px';
+            styleObject.height = '100%';
+        }
+        return styleObject;
     };
 
-    if (side === "top") {
-      styleObject.top = 0;
-      styleObject.left = 0;
-      styleObject.width = "100%";
-      styleObject.height = "4px";
-    } else if (side === "bottom") {
-      styleObject.bottom = 0;
-      styleObject.left = 0;
-      styleObject.width = "100%";
-      styleObject.height = "4px";
-    } else if (side === "left") {
-      styleObject.top = 0;
-      styleObject.left = 0;
-      styleObject.width = "4px";
-      styleObject.height = "100%";
-    } else if (side === "right") {
-      styleObject.top = 0;
-      styleObject.right = 0;
-      styleObject.width = "4px";
-      styleObject.height = "100%";
-    }
-    return styleObject;
-  };
+    const movement = movementSide?.data.uuid === data.uuid ? movementSide.position : null;
+    const style = movement && getMovementStyle(movement);
+    const editorStore = useEditorStore();
+    const isSelection = editorStore.selectBlocks?.filter((block) => {
+        if (block.tagName === 'multiple') return false;
+        const parentBlock = editorStore.findBlock(block.parentId);
+        if (parentBlock) {
+            if (
+                (parentBlock?.tagName === 'checkbox' || parentBlock?.tagName === 'bullet') &&
+                editorStore.selectBlocks.includes(parentBlock)
+            )
+                return false;
+        }
+        return true;
+    });
 
-  const movement =
-    movementSide?.data.uuid === data.uuid ? movementSide.position : null;
-  const style = movement && getMovementStyle(movement);
-  const editorStore = useEditorStore();
-  const isSelection = editorStore.selectBlocks?.filter((block) => {
-    if (block.tagName === "multiple") return false;
-    const parentBlock = editorStore.findBlock(block.parentId);
-    if (parentBlock) {
-      if (
-        (parentBlock?.tagName === "checkbox" ||
-          parentBlock?.tagName === "bullet") &&
-        editorStore.selectBlocks.includes(parentBlock)
-      )
-        return false;
-    }
-    return true;
-  });
+    const BranchTab = () => {
+        let returnComponent;
 
-  const BranchTab = () => {
-    let returnComponent;
+        if (data?.tagName === 'multiple') {
+            returnComponent = (
+                <MultipleBlock data={data} movementSide={movementSide} style={style} isOverlay={isOverlay} />
+            );
+        } else if (data?.tagName === 'image') {
+            returnComponent = <ImageBlock data={data} overlayWidth={overlayWidth} style={style} />;
+        } else if (data?.tagName === 'checkbox') {
+            returnComponent = (
+                <CheckBoxBlock movementSide={movementSide} data={data} overlayWidth={overlayWidth} style={style} />
+            );
+        } else if (data?.tagName === 'bullet') {
+            returnComponent = (
+                <BulletPointBlock movementSide={movementSide} data={data} overlayWidth={overlayWidth} style={style} />
+            );
+        } else {
+            returnComponent = (
+                <EditableBlock data={data} movementSide={movement} overlayWidth={overlayWidth} style={style} />
+            );
+        }
+        return returnComponent;
+    };
 
-    if (data?.tagName === "multiple") {
-      returnComponent = (
-        <MultipleBlock
-          data={data}
-          movementSide={movementSide}
-          changeShowFileUploader={changeShowFileUploader}
-          style={style}
-          isOverlay={isOverlay}
-        />
-      );
-    } else if (data?.tagName === "image") {
-      returnComponent = (
-        <ImageBlock
-          data={data}
-          overlayWidth={overlayWidth}
-          changeShowFileUploader={changeShowFileUploader}
-          style={style}
-        />
-      );
-    } else if (data?.tagName === "checkbox") {
-      returnComponent = (
-        <CheckBoxBlock
-          movementSide={movementSide}
-          data={data}
-          overlayWidth={overlayWidth}
-          style={style}
-        />
-      );
-    } else if (data?.tagName === "bullet") {
-      returnComponent = (
-        <BulletPointBlock
-          movementSide={movementSide}
-          data={data}
-          overlayWidth={overlayWidth}
-          style={style}
-        />
-      );
-    } else {
-      returnComponent = (
-        <EditableBlock
-          data={data}
-          movementSide={movement}
-          overlayWidth={overlayWidth}
-          style={style}
-        />
-      );
-    }
-    return returnComponent;
-  };
-
-  return (
-    <BlockContainer
-      data-uuid={!isOverlay ? data.uuid : null}
-      tagName={data?.tagName}
-    >
-      {BranchTab()}
-      {!isOverlay && isSelection.find((block) => block.uuid === data.uuid) && (
-        <SelectionHalo />
-      )}
-      {editorStore.hoverBlock?.uuid === data.uuid && <HoverBlock />}
-    </BlockContainer>
-  );
+    return (
+        <BlockContainer data-uuid={!isOverlay ? data.uuid : null} tagName={data?.tagName}>
+            {BranchTab()}
+            {!isOverlay && isSelection.find((block) => block.uuid === data.uuid) && <SelectionHalo />}
+            {editorStore.hoverBlock?.uuid === data.uuid && <HoverBlock />}
+        </BlockContainer>
+    );
 };
 
 export default EditBranchComponent;
 
 const BlockContainer = styled.div`
-  display: flex;
-  position: relative;
+    display: flex;
+    position: relative;
 `;
 
 const SelectionHalo = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  background: rgba(35, 131, 226, 0.14);
-  pointer-events: none;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(35, 131, 226, 0.14);
+    pointer-events: none;
 `;
 
 const HoverBlock = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  pointer-events: none;
-  background: rgba(55, 53, 47, 0.1);
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    pointer-events: none;
+    background: rgba(55, 53, 47, 0.1);
 `;
